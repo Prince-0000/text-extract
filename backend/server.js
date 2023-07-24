b0679a45577dd9017343c4cfd42a405c6e91e38f
Prince <princemehta516@gmail.com>
2023-07-25 02:22:31 +0530

    fix code

--- backend/server.js
+++ backend/server.js
@@ -85,7 +85,9 @@ const fs = require('fs');
 
 const app = express();
 const PORT = 5000;
-app.use(cors());
+app.use(cors({
+    origin:'https://text-extract-w.vercel.app/'
+}));
 app.use(express.json());
 
 const airtableApiKey = 'keyxC5y4yJk0lDjJg'; // Replace with your Airtable API key
